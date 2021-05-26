8138af0ee77dc17da2261e520ad9f06413ec293d
Jonas Anton Erhart <csaw4225@zid-gpl.uibk.ac.at>
2021-05-26 19:07:24 +0200

    last changes ex11

--- exercises/ex11/task3/my_malloc.c
+++ exercises/ex11/task3/my_malloc.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// USAGE: LD_PRELOAD=./my_malloc.so <executable (eg.: ls)>
+
 static void* (*default_malloc)(size_t) = NULL;
 
 void print_number(size_t number) {
@@ -13,13 +15,18 @@ void print_number(size_t number) {
 }
 
 void print_size(size_t size) {
-	write(STDOUT_FILENO, "<allocating ", 1);
+	write(STDOUT_FILENO, "<allocating ", 12);
 	print_number(size);
-	write(STDOUT_FILENO, " bytes>", 1);
+	write(STDOUT_FILENO, " bytes>\n", 8);
 }
 
 void* malloc(size_t size) {
 	if (default_malloc == NULL) {
+		// man: RTLD_NEXT: 	Find the next occurrence of the desired symbol in the
+		// 					search order after the current object. (Will search for 'malloc' outside
+		// 					this file)
+		// this may show an error in an IDE, because _GNU_SOURCE needs to be defined, which i did in
+		// my Makefile, so with using 'make', this will work
 		default_malloc = dlsym(RTLD_NEXT, "malloc");
 	}
 	print_size(size);
