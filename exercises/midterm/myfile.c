0ebefb37b818e951321d0145b5df97fcca7bda66
Jonas Erhart <jonas.a.erhart@gmail.com>
2021-06-16 12:15:26 +0200

    midterm skeleton

--- exercises/midterm/myfile.c
+++ exercises/midterm/myfile.c
@@ -9,77 +9,52 @@
 #define KNOBS 2
 
 // tells compiler to not run any optimization on these variables
-volatile int doors_produced;
+volatile int doors_to_produce;
+volatile int knobs_to_produce;
 volatile int knobs_ready;
+volatile int assignments[MAX_THREADS];
 
 pthread_mutex_t mutex;
-// used to interrupt the process when all doors are produced
-pthread_mutex_t quit;
 pthread_cond_t cond;
 
-typedef struct worker {
-	pthread_t* thread;
-	int assigned;
-} worker_t;
-
-int finished(pthread_mutex_t* quit) {
-	switch (pthread_mutex_trylock(quit)) {
-		case 0: pthread_mutex_unlock(quit); return 1;
-		case EBUSY: return 0;
-	}
-	return 1;
-}
-
-void* produce_door(void* arg) {
-	// get laziness value
-	unsigned seed = (unsigned)pthread_self();
-	srand(seed);
-	double lazinenss = (double)rand() / RAND_MAX;
-
-	while (!finished(&quit)) {
+void* produce(void* arg) {
+	int thread_index = *((int*)arg);
+	debug("Got index arg %d", thread_index);
+	for (;;) {
 		// lock the mutex
 		pthread_mutex_lock(&mutex);
-		// check if there are enough knobs to make a door
-		while (knobs_ready < 2)
-			// wait for enough knobs to be available
-			pthread_cond_wait(&cond, &mutex);
-		// make door
-		sleep((10 + lazinenss * 60) / 1000);
-		// knobs are used to make a door
-		knobs_ready -= 2;
-		doors_produced++;
+
+		if (doors_to_produce <= 0) {
+			break;
+		}
+
+		if (assignments[thread_index] == DOORS) {
+			// check if there are enough knobs to make a door
+			while (knobs_ready < 2 && doors_to_produce > 0)
+				// wait for enough knobs to be available
+				pthread_cond_wait(&cond, &mutex);
+			// make door
+			sleep((10 + 0.4 * 60) / 1000);
+			// knobs are used to make a door
+			knobs_ready -= 2;
+			doors_to_produce--;
+		} else {
+			if (knobs_to_produce <= 0 || doors_to_produce <= 0) {
+				break;
+			}
+			sleep((10 + 0.4 * 30) / 1000);
+			// make a knob
+			knobs_ready++;
+			knobs_to_produce--;
+			// if there are two knobs ready, signal to the door workers
+			if (knobs_ready >= 2) {
+				pthread_cond_signal(&cond);
+			}
+		}
 		// CS end
 		pthread_mutex_unlock(&mutex);
 	}
-
-	// exit
-	pthread_exit(NULL);
-}
-
-void* produce_knob(void* arg) {
-	debug("Got quit mutex argument");
-	// get laziness value
-	unsigned seed = (unsigned)pthread_self();
-	srand(seed);
-	double lazinenss = (double)rand() / RAND_MAX;
-
-	debug("Got laziness %ld", lazinenss);
-
-	while (!finished(&quit)) {
-		// lock the critical section
-		pthread_mutex_lock(&mutex);
-		debug("Producing knobs with laziness %ld", lazinenss);
-		// make a knob
-		knobs_ready++;
-		// if there are two knobs ready, signal to the door workers
-		if (knobs_ready >= 2) {
-			pthread_cond_signal(&cond);
-		}
-		// end of critical section
-		pthread_mutex_unlock(&mutex);
-		sleep((10 + lazinenss * 30) / 1000);
-		debug("Sleeping with laziness %ld", lazinenss);
-	}
+	pthread_mutex_unlock(&mutex);
 
 	// exit
 	pthread_exit(NULL);
@@ -98,70 +73,93 @@ int is_number(char number[]) {
 int main(int argc, char* argv[]) {
 	check(argc == 3, "usage: ./factory <enable load balancing> <number of doors to produce>");
 	check(pthread_mutex_init(&mutex, NULL) == 0, "Error initializing mutex");
-	check(pthread_mutex_init(&quit, NULL) == 0, "Error initializing mutex");
 	check(pthread_cond_init(&cond, NULL) == 0, "Error initializing pthread_condition");
 
 	debug("Initialized mutexes");
-
-	pthread_mutex_lock(&quit);
 
 	check(is_number(argv[1]), "Argument '%s' is not a number", argv[1]);
 	check(is_number(argv[2]), "Argument '%s' is not a number", argv[2]);
 
 	int no_doors = atol(argv[2]);
+	int no_knobs = no_doors * 2;
+
+	// assign workers and doors_to_produce
+	pthread_mutex_lock(&mutex);
+	doors_to_produce = no_doors;
+	knobs_to_produce = no_knobs;
+	for (int i = 0; i < MAX_THREADS; i++) {
+		assignments[i] = i < 2 ? KNOBS : DOORS;
+		debug("assigned worker %d", i);
+	}
+	pthread_mutex_unlock(&mutex);
 	debug("Parsed argument no_doors as : %d", no_doors);
 
-	worker_t* workers[MAX_THREADS];
+	pthread_t workers[MAX_THREADS];
 
 	// initialize workers
-	for (int i = 0; i <= 10; i++) {
-		workers[i] = malloc(sizeof(worker_t));
-		debug("Malloced worker %d", i);
-		workers[i]->assigned = KNOBS;
-		debug("Assigned worker %d to KNOBS", i);
-		pthread_create((workers[i]->thread), NULL, produce_knob, NULL);
+	for (int i = 0; i < 10; i++) {
+		int* arg = malloc(sizeof(*arg));
+		arg = &i;
+		check(pthread_create(&workers[i], NULL, &produce, (void*)arg) == 0,
+		      "Error creating pthread");
+
+		debug("Created worker %d", i);
 	}
-	for (int i = 11; i <= MAX_THREADS; i++) {
-		workers[i] = malloc(sizeof(worker_t));
-		debug("Malloced worker %d", i);
-		workers[i]->assigned = DOORS;
-		pthread_create((workers[i]->thread), NULL, produce_door, NULL);
+	for (int i = 10; i < MAX_THREADS; i++) {
+		pthread_create(&workers[i], NULL, &produce, &i);
+
+		debug("Created worker %d", i);
 	}
 
 	// production loop
 	int elapsed = 0;
 	for (;;) {
-		sleep(1);
-		elapsed++;
-		pthread_mutex_lock(&mutex);
-		// print result
-		log_success("Doors produced: %d in %d seconds", doors_produced, elapsed);
-		if (doors_produced >= no_doors) {
-			// signal 'quit'
-			pthread_mutex_unlock(&quit);
+		if (doors_to_produce <= 0) {
+			log_info("Finished!");
 			break;
 		}
-		pthread_mutex_unlock(&mutex);
-	}
-
-	int errors = 0;
-
-	for (int i = 0; i <= MAX_THREADS; i++) {
-		if (pthread_join(*(workers[i]->thread), NULL) != 0) {
-			errors = 1;
+		// assign workers
+		elapsed++;
+		pthread_mutex_lock(&mutex);
+		int doors_produced = no_doors - doors_to_produce;
+		int knobs_produced = no_knobs - knobs_to_produce;
+		debug("Doors still to produce: %d", doors_to_produce);
+		debug("Knobs still to produce: %d", knobs_to_produce);
+		log_info("Producing doors: %.2f /s, knobs: %.2f / s",
+		         ((double)(doors_produced) / (double)elapsed),
+		         ((double)(knobs_produced) / (double)elapsed));
+		if (knobs_produced / doors_produced >= 1.1 || doors_produced == 0) {
+			for (int i = 0; i < MAX_THREADS; i++) {
+				if (assignments[i] == KNOBS) {
+					assignments[i] = DOORS;
+					log_info("\t reassigned worker %d to make doors", i);
+					break;
+				}
+			}
 		}
+		if (doors_produced / knobs_produced >= 1.1) {
+			for (int i = 0; i < MAX_THREADS; i++) {
+				if (assignments[i] == DOORS) {
+					assignments[i] = KNOBS;
+					log_info("\t reassigned worker %d to make knobs", i);
+					break;
+				}
+			}
+		}
+		pthread_mutex_unlock(&mutex);
+
+		sleep(1);
 	}
 
-	check(errors == 0, "There were errors joining phtreads");
-
-	for (int i = 0; i <= MAX_THREADS; i++) {
-		if (workers[i]) free(workers[i]);
+	for (int i = 0; i < MAX_THREADS; i++) {
+		if (pthread_join(workers[i], NULL) == 0) {
+			debug("Joined pthread no %d", i);
+		} else {
+			log_err("Error joining pthread no %d", i);
+		}
 	}
 
 	return 0;
 error:
-	for (int i = 0; i <= MAX_THREADS; i++) {
-		if (workers[i]) free(workers[i]);
-	}
-	return 42;
+	return 1;
 }
